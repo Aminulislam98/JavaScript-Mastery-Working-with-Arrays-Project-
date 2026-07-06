55ed8328d8180aa2932e7d6be94107f9ecb33dd5
Aminul islam <aminurrohman98@gmail.com>
2026-07-06 23:21:42 +0100

    feat: add account type property and implement grouping by account type

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  type: 'Premium',
 };
 
 const account2 = {
@@ -17,6 +18,7 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  type: 'Standard',
 };
 
 const account3 = {
@@ -24,6 +26,7 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  type: 'Premium',
 };
 
 const account4 = {
@@ -31,6 +34,7 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  type: 'Basic',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -257,3 +261,6 @@ const accountMoves = Object.groupBy(accounts, account => {
   }
 });
 console.log(accountMoves);
+
+const groupedAccount = Object.groupBy(accounts, ({ type }) => type);
+console.log(groupedAccount);
